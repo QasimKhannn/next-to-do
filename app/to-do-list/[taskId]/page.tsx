c77bac87b84e58461dd58568d1b8d7dcef73f15c
QasimMalikk <iamqasimk@gmail.com>
2024-06-06 13:46:46 +0500

    /

--- app/to-do-list/[taskId]/page.tsx
+++ app/to-do-list/[taskId]/page.tsx
@@ -24,11 +24,11 @@ const TaskId = async ({ params }: { params: { taskId: string } }) => {
             href={appRoutes.TODOLIST}
           >
             <ArrowLeft className="mr-2 h-4 w-4" />
-            Back to course
+            Back to task
           </Link>
           <div className="flex items-center justify-between">
             <div className="flex flex-col gap-y-2">
-              <h1 className="text-2xl font-medium">course setup</h1>
+              <h1 className="text-2xl font-medium">Task setup</h1>
               <span className="text-sm text-slate-700  dark:text-white">
                 Complete all fields
               </span>
@@ -37,7 +37,7 @@ const TaskId = async ({ params }: { params: { taskId: string } }) => {
         </div>
       </div>
       <div className="mt-6 flex items-center gap-x-2">
-        <h2 className="text-xl">Customize your course</h2>
+        <h2 className="text-xl">Customize your task</h2>
       </div>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
         <div>
